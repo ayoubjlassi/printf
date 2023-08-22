c7ddef376dac04f762cb89d06f1f3b53d386b33a
ayoubjlassi <ayoubjlassi818@gmail.com>
2023-08-22 09:42:55 -0700

    done

--- handle_print.c
+++ handle_print.c
@@ -1,54 +1,49 @@
 #include "main.h"
 /**
- * handle_print - print
- * @f : string
- * @list : list
- * @ind : ind
- * @buffer : input value
- * @flags : flags
- * @width : width
- * @precision : precision
- * @size : size
- * Return: 1 or 2
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
  */
-int handle_print(const char *f, int *ind, va_list list,
-char buffer[], int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
-	f_t f_types[] = {{'c', print_char}, {'s', print_string},
-{'%', print_percent}, {'c', print_char}, {'i', print_int}, {'d', print_int},
-{'b', print_binary}, {'u', print_unsigned},
-{'o', print_octal}, {'x', print_hexadecimal}, {'X', print_hexa_upper},
-{'p', print_pointer}, {'S', print_non_printable},
-{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
-};
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	for (i = 0; f_types[i].f != '\0'; i++)
-		if (f[*ind] == f_types[i].f)
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
 		{
-			return (f_types[i].fn(list, buffer, flags, width, precision, size));
-
-			if (f_types[i].f == '\0')
-			{
-				if (f[*ind] == '\0')
-					return (-1);
-				unkown_len += write(1, "%%", 1);
-				if (f[*ind - 1] == ' ')
-					unkown_len += write(1, " ", 1);
-				else if (width)
-				{
-					while
-					(f[*ind] != ' ' && f[*ind] !=  '%')
-		if
-		(f[*ind] == ' ')
-							return (1);
-
-				}
-				unkown_len += write(1, &f[*ind], 1);
-				return (unkown_len);
-			}
-			return (printed_chars);
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
 		}
-
-
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
 
